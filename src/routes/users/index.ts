3e1833d25205908fdae0b3976445f8f2ce4d55f1
JoseMarshall <hebojosemar@gmail.com>
2021-09-03 21:43:06 +0100

    feat: user routes

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 
+import deleteOneUserController from '../../factories/user/delete-one-user';
 import getAllUsersController from '../../factories/user/get-all-users';
+import getOneUserController from '../../factories/user/get-one-user';
+import updateUserController from '../../factories/user/update-user';
 import { adaptExpressRoute } from '../../main/adapters/express-route-adapter';
 
 export default (router: Router) => {
+  router.get('/:_id', adaptExpressRoute(getOneUserController));
   router.get('/', adaptExpressRoute(getAllUsersController));
+  router.put('/:_id', adaptExpressRoute(updateUserController));
+  router.delete('/:_id', adaptExpressRoute(deleteOneUserController));
 
   return router;
 };
