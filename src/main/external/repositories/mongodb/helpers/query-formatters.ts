05a56cfec6b6f2cc5484743cc5082a1dad29580c
JoseMarshall <hebojosemar@gmail.com>
2021-09-03 16:27:34 +0100

    refactor: format-query-to-regex

--- src/main/external/repositories/mongodb/helpers/query-formatters.ts
+++ src/main/external/repositories/mongodb/helpers/query-formatters.ts
@@ -13,30 +13,9 @@ export const makeCaseInsensitiveRegex = (x: string) => new RegExp(`.*${x}.*`, 'i
 export const formatQueryToRegex = (query: Record<string, string | string[]>) =>
   Object.keys(query).reduce((acc: Record<string, any>, queryKey: string) => {
     const value = query[queryKey];
-    if (typeof value !== 'string') return { ...acc, [queryKey.replace(/>/g, '.')]: value };
-
-    const mapValue = {
-      true: () => {
-        // Assumes the query values follow this format: value1|value2
-        const valuesArray = value.split('|');
-        return {
-          // Transforms the query key 'firstName|lastName' to ['firstName', 'lastName']
-          $or: queryKey.split('|').reduce(
-            (x, key, index) =>
-              x.concat({
-                [key.replace(/>/g, '.')]: makeCaseInsensitiveRegex(valuesArray[index]),
-              }),
-            [] as ReadonlyArray<Record<string, any>>
-          ),
-        };
-      },
-      false: () => ({
-        // Since mongoose-sanitize remove every dot from query, client should use ">" instead,
-        // when intend to search for a nested object key
-        // Transforms field>nestedField1>..>nestedFieldN to field.nestedField1.nestedFieldN
-        [queryKey.replace(/>/g, '.')]: makeCaseInsensitiveRegex(value),
-      }),
+    return {
+      ...acc,
+      [queryKey.replace(/>/g, '.')]:
+        typeof value !== 'string' ? value : makeCaseInsensitiveRegex(value),
     };
-
-    return { ...acc, ...mapValue[String(queryKey.includes('|')) as 'true' | 'false']() };
   }, {});
