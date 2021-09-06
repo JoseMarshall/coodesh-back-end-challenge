5303ce0465fb920d8ab5924899333a060039266e
JoseMarshall <hebojosemar@gmail.com>
2021-09-06 13:57:27 +0100

    feat: aply api key to user routes

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -5,12 +5,13 @@ import getAllUsersController from '../../factories/user/get-all-users';
 import getOneUserController from '../../factories/user/get-one-user';
 import updateUserController from '../../factories/user/update-user';
 import { adaptExpressRoute } from '../../main/adapters/express-route-adapter';
+import { validateApiKey } from '../../main/middleware/api-key-checker';
 
 export default (router: Router) => {
-  router.get('/:_id', adaptExpressRoute(getOneUserController));
-  router.get('/', adaptExpressRoute(getAllUsersController));
-  router.put('/:_id', adaptExpressRoute(updateUserController));
-  router.delete('/:_id', adaptExpressRoute(deleteOneUserController));
+  router.get('/:_id', validateApiKey, adaptExpressRoute(getOneUserController));
+  router.get('/', validateApiKey, adaptExpressRoute(getAllUsersController));
+  router.put('/:_id', validateApiKey, adaptExpressRoute(updateUserController));
+  router.delete('/:_id', validateApiKey, adaptExpressRoute(deleteOneUserController));
 
   return router;
 };
